2b5c84485938584ce4a83ee613b46459e71fa117
Renan veigga <renanveigga@gmail.com>
2026-08-13 18:04:18 -0300

    laço de repetiçao básico

---
+++ fundamentos/laco_repeticao.py
@@ -0,0 +1,30 @@
+# laço de repetição do 1 até o 10
+
+for i in range(1, 11):
+    print(i)
+
+# Exemplo de laço de repetição com número par
+
+for i in range(50):
+    if i % 2 == 0:
+        print(i, "é par")
+
+# Exemplo de laço de repetição com número ímpar
+
+for i in range(50):
+    if i % 2 != 0:
+        print(i, "é ímpar")
+
+#contagem regressiva
+
+for i in range(10, 0, -1):
+    print(i)
+    i -= 1
+
+# Tabuada
+
+numero  = int(input("Digite um número: "))
+
+for i in range(11):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
