dd3adb39550165d7c17ea1ec3662cf80881f0c83
Renan veigga <renanveigga@gmail.com>
2026-08-16 15:29:30 -0300

    verificação e tratamento de erro para o preço do produto

--- task_cadastro_produto/main.py
+++ task_cadastro_produto/main.py
@@ -4,12 +4,11 @@ def exibir_produtos():
          if not produto:
               print("Nenhum item cadastrado")
               return
-
+         
+         print("\nprodutos cadastrados!")
          for nome, preco in produto.items():
-            print("produtos cadastrados!")
             print(f"{len(produto)} produtos cadastrados")
-            print(f"produto: {nome}, preço: R${preco:.2f}")
-            print()
+            print(f"produto: {nome}, preço: R${preco:.2f}\n")
 
 def sair():
     print("Saindo do programa...")
@@ -26,14 +25,31 @@ def menu_cadastro_produto():
     if opcao == "1":
         print("===== CADASTRO DE PRODUTOS =====")
         nome_produto = input("Digite o nome do produto: ")
-        preco_produto = float(input("Digite o preço do produto: "))
-        produto[nome_produto] = preco_produto # Adiciona o produto ao dicionário com o nome como chave e o preço como valor
 
+        while True:
+            try:
+                preco_produto = float(input("Digite o preço do produto: "))
+                
+                # Verificação de preço negativo
+                if preco_produto < 0:
+                    print("Erro: O preço do produto não pode ser negativo! Tente novamente.\n")
+                    continue  # Ignora o resto do laço e volta para o início do 'while'
+                
+                break  # Se o número for válido e maior ou igual a zero, quebra o laço 'while'
+
+            except ValueError:
+                print("\nDigite uma opção válida de preço (use ponto para decimais).")
+
+        
     elif opcao == "2":
         exibir_produtos()   
 
     elif opcao == "3":
         sair()
+
+    else:
+         print("\nOpçao inválida")
+         print("Digite uma opção válida para prosseguir\n")    
  
 while True:
     menu_cadastro_produto()
